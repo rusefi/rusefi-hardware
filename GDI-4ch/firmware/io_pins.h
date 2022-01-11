afee78ebf985109b3b4c56f2fb33b695d34a3730
rusefillc <sdfsdfqsf2334234234>
2022-01-10 23:05:21 -0500

    progress

---
+++ GDI-4ch/firmware/io_pins.h
@@ -0,0 +1,13 @@
+/*
+ * io_pins.h
+ *
+ *  Created on: Jan 10, 2022
+ */
+
+#pragma once
+
+#define LED_BLUE_PORT GPIOC
+#define LED_BLUE_PIN 13
+
+#define LED_GREEN_PORT GPIOA
+#define LED_GREEN_PIN 8
