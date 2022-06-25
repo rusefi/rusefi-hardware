08f63240044db30b4184a89d57dfa01ed9cb9a72
rusefillc <sdfsdfqsf2334234234>
2022-06-25 00:20:52 -0400

    move out of main.cpp

--- GDI-4ch/firmware/mc33816_control.h
+++ GDI-4ch/firmware/mc33816_control.h
@@ -1,6 +1,8 @@
 #include "ch.h"
 #include "hal.h"
 
+#include "mc33816_memory_map.h"
+
 // generic helper methods
 void setup_spi();
 unsigned short recv_16bit_spi();
@@ -9,3 +11,6 @@ unsigned short txrx_16bit_spi(const unsigned short param);
 
 void mcClearDriverStatus();
 unsigned short readId();
+
+unsigned short mcReadDram(MC33816Mem addr);
+void mcUpdateDram(MC33816Mem addr, unsigned short data);
