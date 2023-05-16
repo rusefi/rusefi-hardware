4cae8c7e54a238eb95f6e47e8e789310cd1726f4
rusefi <rusefillc@gmail.com>
2023-05-15 23:12:01 -0400

    GDI4: master does not click 4 injectors #203
    here I revert to microcode version prior to https://github.com/rusefi/rusefi-hardware/pull/139 change
    
    manual testing results: HV changes according to CAN calibration change settings and four injectors click

---
+++ GDI-4ch/firmware/PT2001_LoadData.h
@@ -0,0 +1,20 @@
+
+/*
+ * mc33816_data.h
+ *
+ * @date May 3, 2019
+ * @author Andrey Belomutskiy, (c) 2012-2020
+ */
+
+#pragma once
+
+
+extern unsigned short PT2001_code_RAM1[88];    // CODE RAM CH 1
+extern unsigned short PT2001_code_RAM2[43];    // CODE RAM CH 2
+extern unsigned short PT2001_data_RAM[128];    // DATA RAM
+extern unsigned short PT2001_main_config[29];  // main configurations
+extern unsigned short PT2001_ch1_config[19];   // CH 1 configurations
+extern unsigned short PT2001_ch2_config[19];   // CH 2 configurations
+extern unsigned short PT2001_io_config[44];    // IO configurations
+extern unsigned short PT2001_diag_config[44];  // diag configurations
+
