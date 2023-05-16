06456c4ceb06ddee0dc08f04b3476740be611bfa
rusefi <rusefillc@gmail.com>
2023-05-15 22:37:30 -0400

    still happy

--- GDI-4ch/firmware/mc33816_data.h
+++ GDI-4ch/firmware/mc33816_data.h
@@ -20,12 +20,12 @@ enum {
 	REG_DIAG
 };
 
-extern const unsigned short MC33816_code_RAM1[88];    // CODE RAM CH 1
-extern const unsigned short MC33816_code_RAM2[43];    // CODE RAM CH 2
-extern const unsigned short MC33816_data_RAM[128];    // DATA RAM
-extern const unsigned short MC33816_main_config[29];  // main configurations
-extern const unsigned short MC33816_ch1_config[19];   // CH 1 configurations
-extern const unsigned short MC33816_ch2_config[19];   // CH 2 configurations
-extern const unsigned short MC33816_io_config[44];    // IO configurations
-extern const unsigned short MC33816_diag_config[44];  // diag configurations
+extern unsigned short PT2001_code_RAM1[108];    // CODE RAM CH 1
+extern unsigned short PT2001_code_RAM2[43];    // CODE RAM CH 2
+extern unsigned short PT2001_data_RAM[128];    // DATA RAM
+extern unsigned short PT2001_main_config[29];  // main configurations
+extern unsigned short PT2001_ch1_config[19];   // CH 1 configurations
+extern unsigned short PT2001_ch2_config[19];   // CH 2 configurations
+extern unsigned short PT2001_io_config[53];    // IO configurations
+extern unsigned short PT2001_diag_config[44];  // diag configurations
 
