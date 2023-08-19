72c7d06cec56a038330b7406647478b9ae76a516
andreika-git <andrei.home@gmail.com>
2023-08-19 22:45:32 +0300

    digital-inputs: can_rx + test BENCH_TEST_BOARD_STATUS

--- digital-inputs/firmware/can.cpp
+++ digital-inputs/firmware/can.cpp
@@ -1,11 +1,33 @@
 #include "ch.h"
 #include "hal.h"
+#include "chprintf.h"
+#include "can.h"
+#include "../../ext/libfirmware/can/can_common.h"
+
+
+extern BaseSequentialStream *chp;
 
 static const CANConfig cancfg = {
   CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
   CAN_BTR_SJW(0) | CAN_BTR_TS2(1) |
   CAN_BTR_TS1(8) | CAN_BTR_BRP(6)
 };
+
+
+void receiveBoardStatus(const uint8_t msg[8]) {
+	int boardId = (msg[0] << 8) | msg[1];
+	int numSecondsSinceReset = (msg[2] << 16) | (msg[3] << 8) | msg[4];
+
+	// todo: process board status
+	chprintf(chp, " * BoardStatus: BoardID=%d numSecs=%d\r\n", boardId, numSecondsSinceReset);
+}
+
+void processCanRxMessage(const CANRxFrame& frame) {
+	if (CAN_EID(frame) == BENCH_TEST_BOARD_STATUS) {
+		receiveBoardStatus(frame.data8);
+	}
+
+}
 
 static THD_WORKING_AREA(can_tx_wa, 256);
 static THD_FUNCTION(can_tx, p) {
@@ -26,6 +48,21 @@ static THD_FUNCTION(can_tx, p) {
   }
 }
 
+static THD_WORKING_AREA(can_rx_wa, 256);
+static THD_FUNCTION(can_rx, p) {
+  CANRxFrame rxmsg;
+
+  (void)p;
+  while (true) {
+    msg_t result = canReceiveTimeout(&CAND1, CAN_ANY_MAILBOX, &rxmsg, TIME_INFINITE);
+	if (result != MSG_OK) {
+		continue;
+	}
+
+    processCanRxMessage(rxmsg);
+  }
+}
+
 
 // we are lucky - all CAN pins use the same AF
 #define EFI_CAN_AF 9
@@ -38,4 +75,6 @@ void initCan() {
 
   chThdCreateStatic(can_tx_wa, sizeof(can_tx_wa), NORMALPRIO + 7,
                     can_tx, NULL);
+  chThdCreateStatic(can_rx_wa, sizeof(can_rx_wa), NORMALPRIO + 7,
+                    can_rx, NULL);
 }
--- digital-inputs/firmware/can.h
+++ digital-inputs/firmware/can.h
@@ -1,4 +1,15 @@
 
 #pragma once
 
+// We need these helpers because the frame layout is different on STM32H7
+#ifdef STM32H7XX
+#define CAN_SID(f) ((f).std.SID)
+#define CAN_EID(f) ((f).ext.EID)
+#define CAN_ISX(f) ((f).common.XTD)
+#else
+#define CAN_SID(f) ((f).SID)
+#define CAN_EID(f) ((f).EID)
+#define CAN_ISX(f) ((f).IDE)
+#endif
+
 void initCan();
