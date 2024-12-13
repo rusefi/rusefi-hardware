cbc011dfb504e4429ad3046e880390662d5f83ad
Andrey Gusakov <dron0gus@gmail.com>
2024-12-14 00:26:33 +0300

    Integrate and enable SENT

---
+++ GDI-4ch/firmware/sent.h
@@ -0,0 +1,20 @@
+/*
+ * sent.h
+ *
+ * SENT protocol decoder header
+ *
+ * @date Dec 13, 2024
+ * @author Andrey Gusakov <dron0gus@gmail.com>, (c) 2024
+ */
+
+#pragma once
+
+#include "sent_decoder.h"
+
+/* SENT decoder init */
+void initSent();
+
+/* decoder feed hook */
+void SENT_ISR_Handler(uint8_t channels, uint16_t clocks, uint8_t flags);
+
+int getSentValues(size_t index, uint16_t *sig0, uint16_t *sig1);
